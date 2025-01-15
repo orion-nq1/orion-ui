e5c3351409012121c439a86daead9bf37ce8083c
0xNerd <thejoker3077@gmail.com>
2025-01-14 22:47:32 -0500

    remove Tenji

--- src/components/DashboardHero.vue
+++ src/components/DashboardHero.vue
@@ -2,7 +2,7 @@
     <div class="w-full">
         <div class="mx-auto px-4 h-full flex flex-col gap-6">
             <h1 class="text-xl font-bold text-white mt-12">
-                Welcome back, Tenji.
+                Welcome back!
             </h1>
 
             <DashboardPartial />
@@ -12,11 +12,20 @@
 
 <script>
 import DashboardPartial from '@/partials/dashboard.vue';
+import { mapState } from 'vuex';
 
 export default {
     name: 'DashboardHero',
     components: {
         DashboardPartial,
+    },
+    computed: {
+        ...mapState({
+            userInfo: state => {
+                console.log('UserInfo State:', JSON.stringify(state.userInfo, null, 2));
+                return state.userInfo;
+            }
+        })
     }
 }
 </script>
